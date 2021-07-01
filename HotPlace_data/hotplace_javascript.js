fa2c1cdbb40c274333366265539848d6fec42f2d
smartbgm <46115058+smartbgm@users.noreply.github.com>
2021-07-01 12:15:40 +0900

    Update hotplace_javascript.js

--- HotPlace_data/hotplace_javascript.js
+++ HotPlace_data/hotplace_javascript.js
@@ -5,7 +5,7 @@
 //t1.daumcdn.net/localimg/localimages/07/2018/img/exsearch-ico-search-hover.png
 
 */
-var data_path = JSON.parse("/HotPlace_data/HotPlace_DB_filtered(35-10-20).json")
+var data_path = "/HotPlace_data/HotPlace_DB_filtered(35-10-20).json"
 
 // (37.2804721840256, 127.11467724252604) 기흥구청
 // (37.38279059708606, 127.11882455528438) 분당구청
